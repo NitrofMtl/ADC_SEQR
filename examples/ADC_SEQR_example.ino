7d07dfb58ce6a8937b40a16e6e82ca17598ef3a9
NitrofMtl <o_for@hotmail.com>
2017-02-26 18:59:42 -0500

    adding example

---
+++ examples/ADC_SEQR_example.ino
@@ -0,0 +1,19 @@
+#include <Streaming.h>
+#include <ADC_SEQR.h>
+
+void setup() {
+  Serial.begin(115200); 
+  Adc_Seqr::begin(1); //initiate sequencer, have to be before 'analogReadResolution' call to not reset it to 10
+  analogReadResolution(12);
+  Adc_Seqr::printSetup();
+}
+
+void loop() {
+  unsigned int reading[12];
+  for (int i = 0; i < 12; i++){
+    reading[i] = Adc_Seqr::read(i);
+    Serial << "reading " << i  << " = " << reading[i] << endl;
+  }
+ // Serial << endl;
+    delay(500);
+}
